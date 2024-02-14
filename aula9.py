3fd86efaf686de492392d084116eface2d3fef01
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-14 16:34:14 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+multiplicacao = 10 * 10
+print ('Multiplicação:', multiplicacao)
+
+divisao = 10 / 10
+print('Divisão:', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+resto_divisao = 55 % 2
+print('Resto da divisão:', resto_divisao)
+
+print(16 % 2 == 0)
+print(30 % 2 == 0)
