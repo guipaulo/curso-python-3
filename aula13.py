da20d620bf2c19578c2abdbdae9f0580dcfe7691
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-14 19:44:18 -0300

    add aula13 e aula14

---
+++ aula13.py
@@ -0,0 +1,8 @@
+nome = 'Paulo Guilherme'
+altura = 1.65
+peso = 69.5
+
+imc = peso / (altura*altura)
+
+print('Meu nome é', nome, 'tenho', altura, 'de altura e meu IMC é', imc)
+print(f'Meu nome é {nome}, tenho {altura} de altura e meu IMC é {imc:.2f}')
