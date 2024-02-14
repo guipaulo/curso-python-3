33000d0a8dee1c8da572b3d87a0305c2cb2b63aa
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-14 13:05:30 -0300

    add aula7, aula8

---
+++ aula7.py
@@ -0,0 +1,12 @@
+nome_completo = 'Paulo Guilherme Silva de Araújo'
+soma = 2+2
+int_um = '1'
+print(nome_completo, soma)
+print(int(int_um), type(int(int_um)))
+print(bool(int_um), type(bool   (int_um)))
+
+nome = 'Paulo'
+idade = 23
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Paulo Guilherme'
+sobrenome = 'Silva de Araújo'
+idade = 23
+ano_de_nascimento = 2024 - idade
+maior_de_idade = ano_de_nascimento <= 2006
+altura_metros = 1.65
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_metros)
