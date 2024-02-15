cf8e81ec600ab6272aedc7d09572d78af3dd7a4b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-14 21:35:59 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,11 @@
+#nome = input('Qual o seu nome? ')
+# print(f'Seu nome é {nome}')
+
+num1 = input('Digite um numero: ')
+num2 = input('Digite outro número: ')
+
+int_num1 = int(num1)
+int_num2 = int(num2)
+soma = int_num1+int_num2
+
+print(f'A soma dos números é {soma}')
