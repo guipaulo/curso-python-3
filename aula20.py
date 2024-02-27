bc2bc21236a4d1cf548f204fefc1151f645f4145
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-26 21:28:59 -0300

    Create aula20.py

---
+++ aula20.py
@@ -0,0 +1,7 @@
+PrimeiroValor = input('Digite um valor: ')
+SegundoValor = input('Digite outro valor: ')
+
+if PrimeiroValor > SegundoValor:
+    print(f'{PrimeiroValor} é maior que {SegundoValor}')
+else:
+    print(f'{SegundoValor} é maior que {PrimeiroValor}')
