48e0dbdebc33f83dd82381d8e7741a82ccc18741
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-02-26 21:22:51 -0300

    Create aula19.py

---
+++ aula19.py
@@ -0,0 +1,13 @@
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'b' != 'a'
+
+print(maior)
+print(maior_ou_igual)
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
