967ebfc9f5d6f4ff2784282237199d2c99f1c81d
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-03-01 21:01:11 -0300

    Create aula25.py

---
+++ aula25.py
@@ -0,0 +1,8 @@
+nome = 'Paulo'
+preco = 1000.44003
+variavel = '%s, o preço é %.2f' %(nome, preco)
+
+print(variavel)
+
+hexa = int(input('Digite um número: '))
+print('O hexadecimal de %d é %X' %(hexa, hexa))
