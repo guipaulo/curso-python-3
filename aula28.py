712caacc3cdc931e1b989cf1e73886761bd27b0f
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-03-04 20:38:05 -0300

    Create aula28.py

---
+++ aula28.py
@@ -0,0 +1,16 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Sua idade é {idade}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print(f'Seu nome contém espaços')
+    else:
+        print(f'Seu nome não contém espaços')
+    print(f'Seu nome contém {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print('Você deixou campos vazios!')
