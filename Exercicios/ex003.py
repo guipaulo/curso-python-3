eb1ef75fec43e982618ef063bac6930683dac86b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-03-08 21:32:59 -0300

    add exercícios

---
+++ Exercicios/ex003.py
@@ -0,0 +1,12 @@
+v1, v2, v3 = input().split(' ')
+v4, v5, v6 = input().split(' ')
+v1f = float(v1)
+v2f = float(v2)
+v3f = float(v3)
+v4f = float(v4)
+v5f = float(v5)
+v6f = float(v6)
+
+total = (v2f*v3f)+(v5f*v6f)
+
+print(f'VALOR A PAGAR: R$ {total:.2f}')
---
+++ Exercicios/ex004.py
@@ -0,0 +1,6 @@
+raio = float(input())
+pi = 3.14159
+
+volume_esfera = (4.0/3.0) * pi * raio**3
+
+print(f'VOLUME = {volume_esfera:.3f}')
