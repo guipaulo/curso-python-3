484f16e76d242bd454e473b0b09511e3153fd33c
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2024-03-19 19:26:42 -0300

    Update aula38.py

--- aula38.py
+++ aula38.py
@@ -1,6 +1,9 @@
 linha = 1
 coluna = 1
 
+linha2 = 1
+coluna2 = 1
+
 while linha <= 5:
     coluna = 1
     while coluna <= 5:
@@ -8,3 +11,11 @@ while linha <= 5:
         coluna+=1
 
     linha+=1
+
+while linha2 <= 5:
+    linha2 = 1
+    while linha2 <= 5:
+        print(linha2, coluna2)
+        linha2+=1
+
+    coluna2+=1
